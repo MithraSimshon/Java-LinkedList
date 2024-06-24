82035728210b5b629d75aeb87c1ee8a741fa44de
MITHRA S <167893160+MithraSimshon@users.noreply.github.com>
2024-06-24 16:22:46 +0530

    GetFirst

---
+++ GetFirst.java
@@ -0,0 +1,13 @@
+import java.util.LinkedList;
+public class GetFirst {
+    public static void main(String args[])
+    {
+        LinkedList<String>Technology=new LinkedList<>();
+        Technology.add("c");
+        Technology.add("C++");
+        Technology.add("Java");
+        Technology.add("SQL");
+        Technology.add("Excel");
+        System.out.println(Technology.getFirst());
+    }
+}
