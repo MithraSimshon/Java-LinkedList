73861b0dca79b447b0f36ebe1a6d31508e4d0ef3
MITHRA S <167893160+MithraSimshon@users.noreply.github.com>
2024-06-24 18:41:50 +0530

    Size

---
+++ Size.java
@@ -0,0 +1,21 @@
+import java.util.LinkedList;
+public class Size {
+    public static void main(String args[])
+    {
+        LinkedList<String>Students=new LinkedList<>();
+        Students.add("ECE");
+        Students.add("EEE");
+        Students.add("CSE");
+        Students.add("Bio-Tech");
+        Students.add("Chemical");
+        Students.add("Mech");
+        Students.add("IT");
+        Students.add("AIDS");
+        Students.add("Bio-med");
+        System.out.println(Students.size());
+    }
+}
+
+
+//Output:
+//9
