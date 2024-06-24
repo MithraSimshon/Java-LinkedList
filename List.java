f053fe528b95083b293159477fb149e494b33ab0
MITHRA S <167893160+MithraSimshon@users.noreply.github.com>
2024-06-24 16:05:29 +0530

    AddList

---
+++ List.java
@@ -0,0 +1,17 @@
+import java.util.LinkedList;
+public class List {
+    public static void main(String args[]){
+
+ 
+        LinkedList<String>colors=new LinkedList<>();
+        colors.add("Green");
+        colors.add("Blue");
+        colors.add("Yellow");
+        colors.add("Pink");
+        colors.addFirst("Black");
+        System.out.println(colors);
+      }   
+         
+
+ 
+}
